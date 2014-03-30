e8b2eb6c7e4ca1cc64dc73e1bfc807b02cc65ac1
Louis Vela <lvela@enfocussolutions.com>
2014-03-30 12:24:03 -0500

    Added substition and writing to file.

--- rip.rb
+++ rip.rb
@@ -1,12 +1,20 @@
+#!/usr/bin/env ruby
+
 require 'ripper'
 require 'pp'
-require 'sorcerer'
 
-#sexp = Ripper::SexpBuilder.new(File.read(ARGV[0])).parse
 lexed =  Ripper.lex(File.read(ARGV[0]))
-#pp sexp
-#puts Sorcerer.source(sexp, indent: true)
 
-lexed.each do |i|
-  print i[2]
+s, from, to = ARGV[1].split '/'
+
+lexed.each do |x| 
+  if x[1] == :on_ident
+    x[2].sub!(/#{from}/, to)
+  end
+end
+
+File.open('test.rb', 'w') do |file|
+  lexed.each do |i|
+    file.write i[2]
+  end
 end
